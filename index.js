1c921a4c0736167f82aaf011743fdbcca2af49c8
Robert McCary <robert.mccary@yahoo.com>
2019-06-26 01:20:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const baseURL = 'https://api.github.com';
 const user = '<YOUR_USERNAME>';
+const token = '';
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return '';
+  return token;
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(response=> response.json())
+  .then(reply=> showResults(reply));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `a href=$(json.html_url)>${
+    json.html_url
+  }</a>`; 
 }
 
 function createIssue() {
